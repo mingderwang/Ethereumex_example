f4b268f8818d6c1ca45af31ee09f9971310290cd
Ming-der Wang <mingderwang@gmail.com>
2018-02-08 11:23:52 +0800

    test Ethereumex

---
+++ lib/example.ex
@@ -0,0 +1,18 @@
+defmodule Example do
+  @moduledoc """
+  Documentation for Example.
+  """
+
+  @doc """
+  Hello Ming.
+
+  ## Examples
+
+      iex> Example.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
